4066c1d46d60656e58a87fc728066bceb2d572f4
Sabin Sapkota <ssapkota20@my.whitworth.edu>
2019-08-01 18:35:40 -0700

    Router to send email to database

--- halbr_backend/routes/email.js
+++ halbr_backend/routes/email.js
@@ -5,9 +5,13 @@ var email = require('../database/models/email');
 
 emailRouter.route('/add/post').post(function (req, res) {
     var email = new email(req.body);
-   
-    
-
+    email.save()
+    .then (email =>{
+            res.status(200).json({Item:'Item added successfully'})
+    })
+            .catch (err => {
+            res.status(400).send("unable to save to database");
+            });
 })
 
 
